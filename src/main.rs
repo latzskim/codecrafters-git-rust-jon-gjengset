a2799aab2ccf9abfe88e49e62fb8e712eea84f97
latzskim <karolmiksztal@gmail.com>
2024-03-17 20:53:17 +0100

    chore: init command pregenerated

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Init,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("initialized git repository")
+        }
+    }
+}
